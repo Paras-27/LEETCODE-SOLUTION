711fa77157900709a86558d03b64a97653104d55
Paras-27 <105578635+Paras-27@users.noreply.github.com>
2023-07-18 13:11:51 +0530

    Time: 20 ms, Memory: 15.6 MB - LeetHub

---
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        vector<int> ans(2,-1);
+        while(left<=right){
+            if(nums[left]+nums[right]>target){
+                right--;
+            }
+            else if(nums[left]+nums[right]<target){
+                left++;
+            }
+            else{
+                ans[0]=left+1;
+                ans[1]=right+1;
+                break;
+            }
+        }
+        return ans;
+    }
+};
