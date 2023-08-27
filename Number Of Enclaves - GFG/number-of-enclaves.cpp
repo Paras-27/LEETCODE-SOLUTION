8d2df7b187aebb90db21c414331cca5aa8176b22
Paras-27 <105578635+Paras-27@users.noreply.github.com>
2023-08-27 12:20:57 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -10,19 +10,20 @@ using namespace std;
 
 class Solution {
   public:
-    void dfs(vector<vector<int>> &grid,int i,int j,vector<vector<int>> &visited,bool &check,int &store){
+    int  dfs(vector<vector<int>> &grid,int i,int j,vector<vector<int>> &visited,bool &check){
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || visited[i][j] || grid[i][j]!=1){
-            return;
+            return 0;
         }
         if(i==0 || j==0 || i==grid.size()-1 || j==grid[0].size()-1){
             check=false;
         }
         visited[i][j]=1;
-        store++;
-        dfs(grid,i+1,j,visited,check,store);
-        dfs(grid,i-1,j,visited,check,store);
-        dfs(grid,i,j+1,visited,check,store);
-        dfs(grid,i,j-1,visited,check,store);
+        int count = 1;
+        count+=dfs(grid,i+1,j,visited,check);
+        count+=dfs(grid,i-1,j,visited,check);
+        count+=dfs(grid,i,j+1,visited,check);
+        count+=dfs(grid,i,j-1,visited,check);
+        return count;
     }
     
     int numberOfEnclaves(vector<vector<int>> &grid) {
@@ -34,9 +35,7 @@ class Solution {
             for(int j=0;j<m;j++){
                 bool check=true;
                if(!visited[i][j] && grid[i][j]==1){
-                   int store=0;
-                   dfs(grid,i,j,visited,check,store);
-                //   cout<<store<<" ";
+                   int store=dfs(grid,i,j,visited,check);
                    if(check){
                        ans+=store;
                    }
