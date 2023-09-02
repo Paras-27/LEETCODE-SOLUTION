7ad72dd7cde55b4e8806ffcc708880c97777f186
Paras-27 <105578635+Paras-27@users.noreply.github.com>
2023-09-02 11:36:28 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,22 +10,32 @@ using namespace std;
 
 class Solution{
   public:
-    int help(int price[],int j,int i,int n,vector<vector<int>> &dp){
-        if(i==n+1){
-            return 0;
-        }
-        if(dp[j][i]!=-1){
-            return dp[j][i];
-        }
-        int take=help(price,i,i+1,n,dp)+price[i-j-1];
-        int notake=help(price,j,i+1,n,dp);
-        return dp[j][i]=max(take,notake);
-    } 
+    // int help(int price[],int j,int i,int n,vector<vector<int>> &dp){
+    //     if(i==n+1){
+    //         return 0;
+    //     }
+    //     if(dp[j][i]!=-1){
+    //         return dp[j][i];
+    //     }
+    //     int take=help(price,i,i+1,n,dp)+price[i-j-1];
+    //     int notake=help(price,j,i+1,n,dp);
+    //     return dp[j][i]=max(take,notake);
+    // } 
     int cutRod(int price[], int n) {
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        return help(price,0,1,n,dp);
+        vector<int> prev(n+2,0);
+        for(int i=n;i>=1;i--){
+            vector<int> curr(n+2,0);
+            for(int j=n;j>=0;j--){
+                int take=prev[i]+price[i-j-1];
+                int notake=prev[j];
+                curr[j]=max(take,notake);
+            }
+            prev=curr;
+        }
+        return prev[0];
     }
 };
+
 
 //{ Driver Code Starts.
 
